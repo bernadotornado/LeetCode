97afb5eda88b45223ee41e68fbb5d35e2ea34e9c
ADMIN\bszha <sze19728@spengergasse.at>
2022-12-04 00:00:28 +0100

    inital commit

---
+++ 13. Roman to Integer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LeetCode;
+
+namespace _13._Roman_to_Integer
+{
+    class Solution
+    {
+        static public int RomanToInt(string s)
+        {
+            int result = 0;
+            for (int i = 0; i <s.Length; i++)
+            {
+                var current = s[i];
+                var next = '?';
+                var n = i + 1;
+                next = s[n> s.Length -1 ? s.Length-1 : n];
+
+                result += current switch
+                {
+                    'I' => 1   * (next == 'V' || next == 'X' ? -1 : 1), 
+                    'X' => 10  * (next == 'L' || next == 'C' ? -1 : 1), 
+                    'C' => 100 * (next == 'D' || next == 'M' ? -1 : 1), 
+                    'V' => 5,
+                    'L' => 50,
+                    'D' => 500,
+                    'M' => 1000,
+                };
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Common.StartBenchmark();
+
+            Console.WriteLine(RomanToInt("MDCCCXLIX"));
+            
+            Common.EndBenchmark();
+
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCode
+{
+    public class Common
+    {
+        static Stopwatch sw = new Stopwatch();
+        static public void StartBenchmark()
+        {
+            sw.Start();
+        }
+        static public void EndBenchmark()
+        {
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
